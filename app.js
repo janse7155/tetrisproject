2c1fc661ee1563e5e48b6a1ef60125141220fc99
JRSeabaugh1s <JRSeabaugh1s@capetigers.com>
2022-12-01 09:10:37 -0600

    add main files

---
+++ app.js
@@ -0,0 +1,7 @@
+let canvas;
+let ctx;
+let gBArrayHeight = 20;
+let gBArrayWidth = 12
+let startX = 4;
+let startY = 0;
+let coordinateArray = [...Array(gBArrayHeight)].map(e => Array(gBArrayWidth).fill())
